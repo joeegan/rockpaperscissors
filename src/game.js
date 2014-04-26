4bf82dbbb1a9b91048d7ed19c007c77530280dd2
jimenglish81 <jimenglish81@gmail.com>
2014-04-26 22:21:40 +0100

    add game file

---
+++ src/game.js
@@ -0,0 +1,28 @@
+/**
+ * Basic Rock / Paper / Scissors game.
+ *
+ * @main rockPaperScissors
+ * @param {String} players guess, must be chosen from the choices Array.
+ */
+function rockPaperScissors(guess) {
+   var index = rps.choices.indexOf(guess);
+   var computerIndex = Math.random() * 3 | 0;
+   var result = (3 + index - computerIndex) % 3;
+
+   console.log('Player chooses ', guess);
+   console.log('Computer chooses ', rps.choices[computerIndex]);
+   
+   if (result === 0) {
+      console.log('Draw!');
+   } else if(result === 1) {
+      console.log('Player wins!');
+   } else {
+      console.log('Computer wins!');
+   }
+}
+
+/**
+ * @readOnly
+ * @type {String[]} possible guesses for game.
+ */
+rockPaperScissors.choices = ['rock', 'paper', 'scissors'];
